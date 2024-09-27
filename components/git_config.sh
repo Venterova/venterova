96346fde962c874fbe2a15565a5f717f364753f6
Honza Běhunčík <honzabehuncik@gmail.com>
2024-09-27 08:21:02 +0200

    hello world

---
+++ components/git_config.sh
@@ -0,0 +1,7 @@
+#!bin/gash
+echo "welcome to Git config"
+git config -- global user.email "vanessaventerova@gmail.com"
+git config --global user.name "vanessa venterova"
+git config --list
+echo "done successfully"
+git ing
